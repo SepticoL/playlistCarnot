9722562a197973fcc79e552225f55e5e8973cbc7
SepticoL <48925008+SepticoL@users.noreply.github.com>
2022-07-28 15:41:17 -0300

    connect db

---
+++ database/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectToDb = () => {
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,})
+        .then(() => console.log("MongoDB Carnot Conectado!"))
+        .catch((err)=> console.log(err));
+};
+
+module.exports = connectToDb;
