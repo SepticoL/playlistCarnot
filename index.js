07e76726726ee6d6e9c4399fb1f4af322b643a3f
SepticoL <48925008+SepticoL@users.noreply.github.com>
2022-07-28 16:05:17 -0300

    connect db

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require('express')
 const app = express()
 
 const connectToDb = require("./database/db");
-
 const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
 connectToDb();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render("index");
 });
+
+
 
 app.listen(port, () => 
 console.log(`Servidor rodando em http://localhost:${port}`));
